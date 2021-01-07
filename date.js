2b90acca64c18e926e51ee8e8da7137a7a610932
Sreecharan KP <43923278+sreecharankp@users.noreply.github.com>
2021-01-07 12:51:34 +0530

    Todo Application

---
+++ date.js
@@ -0,0 +1,25 @@
+module.exports.getDate = function() {
+  let today = new Date();
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+
+  return day;
+}
+
+module.exports.getDay = function() {
+  let today = new Date();
+
+  let options = {
+    weekday: "long"
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+
+  return day;
+}
